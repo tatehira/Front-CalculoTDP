98f9a34a8d08e21eadf5089f6e70bd9ee7efc706
Wendrew nick costa tatehira <niickcostta00@outlook.com>
2023-06-23 19:00:20 -0400

    Update projeto

--- src/components/EnumSelect.js
+++ src/components/EnumSelect.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const EnumSelect = ({ label, options, value, onChange }) => {
   return (
-    <div className="select-wrapper">
-      <h3>{label}</h3>
-      <select value={value} onChange={onChange}>
-        {options.map((option) => (
+    <div>
+      <label>{label}</label>
+      <select value={value || ''} onChange={onChange}>
+        {options.map(option => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
